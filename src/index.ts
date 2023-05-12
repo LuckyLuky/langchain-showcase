1f68435a2c18c32f65934860f789160309ea4fec
Lukas Havlicek <8luckyluky8@gmail.com>
2023-05-12 13:29:43 +0200

    Use inquirer to select features and models

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
+import inquirer from "inquirer";
 import { features } from "./features";
 import { chatModels } from "./chatModels";
+import { log } from "./utils";
 
-const chatOpenAI = chatModels.openAI();
+inquirer
+  .prompt([
+    {
+      name: "feature",
+      type: "list",
+      message: "Select feature",
+      choices: () => Object.keys(features),
+    },
+    {
+      name: "chatModel",
+      type: "list",
+      message: "Select chat model",
+      choices: () => Object.keys(chatModels),
+    },
+  ])
+  .then(({ feature, chatModel }) => {
+    if (!features[feature] || !chatModels[chatModel]) {
+      throw new Error("Invalid input");
+    }
 
-features.fewShot(chatOpenAI);
+    log(`Running ${feature} with ${chatModel}`);
+
+    return features[feature](chatModels[chatModel]());
+  });
